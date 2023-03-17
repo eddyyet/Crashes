61e4491e692aa9652a2267737265be01e97057fe
eddyyet <yet.eddy@gmail.com>
2023-03-17 20:52:57 +0800

    ESLint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import Stack from '@mui/material/Stack';
-import { Chart } from './chart';
+import './App.css'
+import React from 'react'
+import Stack from '@mui/material/Stack'
+import { Chart } from './chart'
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <Stack spacing={1.5} sx={{ width:'800px'}}>
+      <Stack spacing={1.5} sx={{ width: '800px' }}>
         <Chart>1</Chart>
         <Chart>2</Chart>
         <Chart>3</Chart>
       </Stack>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/chart.js
+++ src/chart.js
@@ -2,12 +2,12 @@ import { styled } from '@mui/material/styles'
 import { Box } from '@mui/material'
 
 export const Chart = styled(Box)(({ theme }) => ({
-    // [theme.breakpoints.up('sm')]: { width: '552px' },
-    backgroundColor: '#28292a',
-    '&:hover': {backgroundColor: '#2d2f31'},
-    borderRadius: '1rem',
-    padding: '1.5rem',
-    textAlign: 'left',
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
+  // [theme.breakpoints.up('sm')]: { width: '552px' },
+  backgroundColor: '#28292a',
+  '&:hover': { backgroundColor: '#2d2f31' },
+  borderRadius: '1rem',
+  padding: '1.5rem',
+  textAlign: 'left',
+  fontSize: '1.5rem',
+  fontWeight: 'bold'
 }))
