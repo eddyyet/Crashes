e3df1b017990a193c6785ae84eb2ed7711ce2bc5
eddyyet <yet.eddy@gmail.com>
2023-04-05 20:45:56 +0800

    Data (environment - injury, better logic)

--- src/utils/NetworkData.js
+++ src/utils/NetworkData.js
@@ -1,14 +1,14 @@
 const NetworkData = (data) => {
   nodes: [
-    { id: "Moderate injury", x: -100, y: 0 },
-    { id: "No injury", x: 0, y: 0 },
-    { id: "Severe injury", x: 100, y: 0 },
+    { id: "Moderate injury", label: "Moderate injury", x: 0, y: 0 },
+    { id: "No injury", label:  "No injury", x: 0, y: 100 },
+    { id: "Severe injury", label: "Severe injury", x: 0, y: -100 },
     ...Object.keys(data)
       .map((key, i) => ({
         id: key.split(",")[0],
         label: key.split(",")[0],
-        x: 100 * Math.cos(((2 * Math.PI) / (Object.keys(data).length - 3)) * i),
-        y: 100 * Math.sin(((2 * Math.PI) / (Object.keys(data).length - 3)) * i),
+        x: 300 * Math.cos(((2 * Math.PI) / (Object.keys(data).length - 3)) * i),
+        y: 300 * Math.sin(((2 * Math.PI) / (Object.keys(data).length - 3)) * i),
       }))
   ],
   
