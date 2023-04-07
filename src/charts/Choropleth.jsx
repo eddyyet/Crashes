60109fd90023d6df3a9ff928d7e30e949a403e84
manleungka <manleungka@gmail.com>
2023-04-08 02:19:07 +0800

    map mouseover

--- src/charts/Choropleth.jsx
+++ src/charts/Choropleth.jsx
@@ -3,10 +3,54 @@ import React from 'react'
 // import { MapContainer, TileLayer } from 'react-leaflet'
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 // mport L from 'leaflet'
-// import side from '../data/custom.geo.50.json'
 import side from '../data/chicago_side.json'
 import './Choropleth.css'
 import 'leaflet/dist/leaflet.css'
+
+function onMouseOver (event) {
+  const layer = event.target
+  layer.setStyle({
+    weight: 3,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+  })
+  layer.bringToFront()
+}
+
+function onMouseOut (event) {
+  event.target.setStyle(style(event.target.feature))
+  /*
+  event.target.bringToFront()
+  event.target.setStyle({
+    fillColor: '#FC4E2A',
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    dashArray: '2',
+    fillOpacity: 0.7
+  })
+  */
+}
+
+function onEachFeature (feature, layer) {
+  layer.on({
+    mouseover: onMouseOver,
+    mouseout: onMouseOut
+  })
+}
+
+function style () {
+  return {
+    fillColor: '#FC4E2A',
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    dashArray: '2',
+    fillOpacity: 0.7
+  }
+}
+
 export default function CrashBySide () {
   return (
     /*
@@ -25,7 +69,7 @@ export default function CrashBySide () {
       <h1>Choropleth</h1>
       <MapContainer center={[41.881832, -87.623177]} zoom={10} scrollWheelZoom={false}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <GeoJSON data={side} />
+        <GeoJSON data={side} style={style} onEachFeature={onEachFeature} />
         </MapContainer>
     </div>
   </div>
