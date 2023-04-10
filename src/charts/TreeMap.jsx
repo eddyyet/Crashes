10039493c9c9e170514570d39f12ef55b87b9451
eddyyet <yet.eddy@gmail.com>
2023-04-10 21:01:31 +0800

    TreeMap (text)

--- src/charts/TreeMap.jsx
+++ src/charts/TreeMap.jsx
@@ -4,11 +4,10 @@ import filter from '../utils/filter'
 import TreeMapData from '../utils/TreeMapData'
 import causeData from '../data/crash_cause.json'
 
-const CustomLabel = ({ node, ...props }) => (
-  <div style={{ textAlign: 'center', lineHeight: '18px' }}>
-    <strong>{node.id}</strong>
-    <br />
-    <span>{node.value}</span>
+const labelStyle = (node) => (
+  <div style={{ textAlign: 'center' }}>
+    <div style={{ width: '150px', whiteSpace: 'normal' }}>{node.id}</div>
+    <div style={{ fontSize: '30px' }}>{node.value}</div>
   </div>
 )
 
@@ -18,15 +17,15 @@ export default function TreeMap (props) {
 
   return (
     <ResponsiveTreeMapHtml
-      height={400}
+      height={600}
       data={treeMapData}
       identity='cause'
       value='count'
+      orientLabel={false}
       colors={{ scheme: 'nivo' }}
-      label={ (node) => (<div><div width='2'>{node.id}</div><div>{node.value}</div></div>) }
-      labelComponent={ props => <CustomLabel {...props} /> }
+      label={labelStyle}
       labelSkipSize={20}
-      labelTextColor={{ from: 'color', modifiers: [['brighter', 1.5]] }}
+      labelTextColor={{ from: 'color', modifiers: [['brighter', 2]] }}
       borderWidth={1}
       borderColor={{ from: 'color', modifiers: [['darker', 0.3]] }}
     />
