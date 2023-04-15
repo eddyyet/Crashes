a50fd464dd24d9e3ea089196ee61926e7531827f
eddyyet <yet.eddy@gmail.com>
2023-04-15 19:13:03 +0800

    Scatter (color and size)

--- src/charts/ScatterPlot.jsx
+++ src/charts/ScatterPlot.jsx
@@ -7,42 +7,52 @@ import dayTimeData from '../data/crash_day_time.json'
 export default function ScatterPlot (props) {
   const filteredData = filter(dayTimeData, props.year, props.side)
   const scatterPlotData = ScatterPlotData(filteredData)
-  const maxCrashes = Math.max(...scatterPlotData.data.map(d => d.crashes))
-  const minCrashes = Math.min(...scatterPlotData.data.map(d => d.crashes))
+  const colors = scatterPlotData.map(d => d.data[0].color).flat()
+  console.log(scatterPlotData)
+
+  // function scale () {}
+  // scale.domain = () => {
+  //   const _colors = colors.slice(0)
+
+  //   return () => {
+  //     return _colors.shift()
+  //   }
+  // }
 
   return (
     <ResponsiveScatterPlot
-      height={500}
+      height={550}
       width={350}
-      margin={{ top: 50, right: 50, bottom: 40, left: 40 }}
-      data={[scatterPlotData]}
-      nodeSize={{ key: 'data.crashes', values: [minCrashes, maxCrashes], sizes: [8, 30] }}
+      margin={{ top: 60, right: 50, bottom: 40, left: 68 }}
+      data={scatterPlotData}
+      nodeSize={{ key: 'data.radius', values: [0, 1], sizes: [0, 40] }}
       xScale={{ type: 'point' }}
-      yScale={{ type: 'linear', min: 0, max: 23, reverse: true }}
+      yScale={{ type: 'point' }}
       axisTop={{
         orient: 'top',
         tickValues: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        tickSize: 5,
-        tickPadding: 5,
+        tickSize: 0,
+        tickPadding: 25,
         tickRotation: 0,
-        legend: 'Day',
+        legend: 'Day of week',
         legendPosition: 'middle',
-        legendOffset: -35
+        legendOffset: -50
       }}
       axisLeft={{
         orient: 'left',
-        tickSize: 5,
-        tickPadding: 5,
+        ticksValues: 1,
+        tickSize: 0,
+        tickPadding: 35,
         tickRotation: 0,
         legend: 'Time (hour)',
         legendPosition: 'middle',
-        legendOffset: -35
+        legendOffset: -63
       }}
       axisBottom={null}
       enableGridX={false}
       enableGridY={false}
       theme={{ axis: { ticks: { text: { fill: '#999999' } }, legend: { text: { fill: '#999999' } } } }}
-      colors={{ datum: 'color' }}
+      colors={colors}
     />
   )
 }
--- src/utils/ScatterPlotData.js
+++ src/utils/ScatterPlotData.js
@@ -1,6 +1,7 @@
 export default function ScatterPlotData (data) {
-  const scatterPlotData = []
+  const nivoInputData = []
   const combination = {}
+  let maxCrashes = -Infinity
 
   for (const key in data) {
     const [x, y, type] = key.split(',')
@@ -8,34 +9,29 @@ export default function ScatterPlotData (data) {
     const dayTime = `${x},${y}`
 
     if (!combination[dayTime]) {
-      combination[dayTime] = { x, y: +y, crashes: 0, injured: 0, injuryRate: 0, color: '' }
+      combination[dayTime] = { x, y: +y, crashes: 0, injured: 0, injuryRate: 0, radius: 1, color: '' }
     }
 
     if (type === 'Total') {
       combination[dayTime].crashes = value
+      if (value > maxCrashes) { maxCrashes = value }
     } else {
       combination[dayTime].injured = value
     }
   }
 
   for (const dayTime in combination) {
+    combination[dayTime].radius = Math.sqrt(combination[dayTime].crashes / maxCrashes)
+
     const injuryRateNum = combination[dayTime].injured / combination[dayTime].crashes
     combination[dayTime].injuryRate = (injuryRateNum * 100).toFixed(1) + '%'
 
     const saturation = (Math.min(Math.max(injuryRateNum - 0.09, 0) * 20, 0.9) * 100).toFixed(0) + '%'
-    combination[dayTime].color = `hsl(10, ${saturation}, 55%)`
+    const alpha = (Math.min(Math.max(injuryRateNum - 0.09, 0) * 12 + 0.4, 1) * 100).toFixed(0) + '%'
+    combination[dayTime].color = `hsla(10, ${saturation}, 55%, ${alpha})`
 
-    scatterPlotData.push(combination[dayTime])
+    nivoInputData.push({ id: dayTime, data: [combination[dayTime]] })
   }
 
-  // const maxCrashes = scatterPlotData.reduce(
-  //   (max, obj) => (obj.crashes > max ? obj.crashes : max),
-  //   -Infinity
-  // )
-  // const minCrashes = scatterPlotData.reduce(
-  //   (min, obj) => (obj.crashes < min ? obj.crashes : min),
-  //   Infinity
-  // )
-
-  return { data: scatterPlotData }
+  return nivoInputData
 }
